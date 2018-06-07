68cb30bf8d7d4fa6867f388e41bc2c3d9d08d43d
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-06-06 18:27:05 -0700

    Added Ship horizontal and vertical movement

---
+++ Rail Shooter Game/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 6f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 6f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRange = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRange = 2f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float newX = transform.localPosition.x + xOffset;
+        float clampedX = Mathf.Clamp(newX, -xRange, xRange);
+        transform.localPosition = new Vector3(clampedX, transform.localPosition.y, transform.localPosition.z);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float newY = transform.localPosition.y + yOffset;
+        float clampedY = Mathf.Clamp(newY, -yRange, yRange);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedY, transform.localPosition.z);
+    }
+}
