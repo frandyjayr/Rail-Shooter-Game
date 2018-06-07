9de00cbaf31ef043ced2755adae224b40a695258
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-06-06 19:02:54 -0700

    Fixed movement bugs

--- Rail Shooter Game/Assets/Player.cs
+++ Rail Shooter Game/Assets/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour {
     [Tooltip("In ms^-1")] [SerializeField] float xRange = 4f;
     [Tooltip("In ms^-1")] [SerializeField] float yRange = 2f;
 
+    [SerializeField] float positionPitchFactor = -7f;
+    [SerializeField] float positionYawFactor = 7f;
+
+    float xThrow, yThrow;
 
     // Use this for initialization
     void Start () {
@@ -17,13 +21,27 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation() {
+        float pitch = transform.localPosition.y * positionPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
+    }
+
+    private void ProcessTranslation() {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float newX = transform.localPosition.x + xOffset;
         float clampedX = Mathf.Clamp(newX, -xRange, xRange);
         transform.localPosition = new Vector3(clampedX, transform.localPosition.y, transform.localPosition.z);
 
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
         float newY = transform.localPosition.y + yOffset;
         float clampedY = Mathf.Clamp(newY, -yRange, yRange);
