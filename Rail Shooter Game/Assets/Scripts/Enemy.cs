04e42e258d7c416109a94928b593fe48078bfeba
Frandy Jay-R Usi <frandyjayr@gmail.com>
2018-06-12 22:43:20 -0700

    Enemy Ships can now destroy their own game objects after collision

---
+++ Rail Shooter Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<BoxCollider>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other) {
+        print("Destroyed Enemy" + gameObject.name);
+        Destroy(gameObject);
+    }
+}
